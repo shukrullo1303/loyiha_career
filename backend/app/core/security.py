df2f54b443791f668ebf72c5f7e62720f72bce59
shukrullo1303 <bekbek791@gmail.com>
2026-02-16 22:45:41 +0500

    init

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -12,7 +12,7 @@ from sqlalchemy.orm import Session
 
 from app.core.config import settings
 from app.core.database import get_db
-from backend.app.models.user import User
+from app.models.user import User
 from app.schemas.user import TokenData
 
 # Парол контексти
